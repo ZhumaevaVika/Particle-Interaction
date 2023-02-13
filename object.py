7d5bbed9a40f3a5d3899eacff0cd0c7c4bab458f
KasyanovIvan2004 <mr.potato800@gmail.com>
2023-02-13 20:03:05 +0300

    first commit

---
+++ object.py
@@ -0,0 +1,13 @@
+class Object:
+
+    def __init__(self):
+        self.m = 1
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.vx = 0
+        self.vy = 0
+        self.vz = 0
+        self.ax = 0
+        self.ay = 0
+        self.az = 0
